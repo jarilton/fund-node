3cf53bb1606b24941d506b97bcf19e456a14e3bc
Jamal <jarilton.pereira@m2center.com.br>
2022-07-15 16:48:00 -0300

    Conflitos resolvido

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-<<<<<<< Updated upstream
     return res.json({ message: "Bem vindos ao curso de node js!!" });
-=======
-    return res.json({ message: "teste!!" });
->>>>>>> Stashed changes
 })
 
 app.listen(3306); // serve para startar a aplicação
