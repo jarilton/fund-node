b778d7a5e122ed9f7ffc4f93f8dfe6d61bb30d7e
Jamal <jarilton.pereira@m2center.com.br>
2022-07-15 22:57:49 -0300

    Tipos de parametros

--- src/index.js
+++ src/index.js
@@ -2,8 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: "Bem vindos ao curso de node js!!" });
+app.use(express.json()); 
+
+app.get('/courses', (req, res) => {
+    const query = req.query;
+    console.log(query);
+    return res.json(["Curso: 1", "Curso: 2", "Curso: 3"])
 })
+
+app.post('/courses', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    return res.json(["Curso: 1", "Curso: 2", "Curso: 3", "Curso: 4"])
+})
+
+app.put('/courses/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    return res.json(["Curso: 6", "Curso: 2", "Curso: 3", "Curso: 4"])
+})
+
+app.patch('/courses/:id', (req, res) => {
+    return res.json(["Curso: 6", "Curso: 2", "Curso: 3", "Curso: 4"])
+})
+
+app.delete('/courses/:id', (req, res) => {
+    return res.json(["Curso: 1", "Curso: 2", "Curso: 4"])
+})
+
 
 app.listen(3306); // serve para startar a aplicação
