d88f0184ec056bdfbd9a8c6b05069b66c25776d1
Jarilton <jariltonpereira02@hotmail.com>
2022-07-17 11:19:37 -0300

    Middlewares

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require("uuid");
 
@@ -8,14 +8,24 @@ app.use(express.json());
 
 const customers = [];
 
-app.get('/statement', (req, res) => {
+//middleware
+function verifyIfExistAccountCPF(req, res, next) {
     const { cpf } = req.headers;
 
     const customer = customers.find(customer => customer.cpf === cpf);
 
-    if(!customer) {
-        return response.status(400).json({error: 'Customer not found'})
+    if (!customer) {
+        return response.status(400).json({ error: 'Customer not found' })
     }
+
+    request.customer = customer;
+
+    return next();
+}
+
+
+app.get('/statement', verifyIfExistAccountCPF, (req, res) => {
+    const { customer } = req; 
 
     return response.json(customer.statement);
 
@@ -26,7 +36,7 @@ app.post('/account', (req, res) => {
 
     const customerAlreadyExist = customers.some((customer) => customer.cpf === cpf);
 
-    if(customerAlreadyExist) {
+    if (customerAlreadyExist) {
         return response.status(400).json({ error: "Customer already exists!" });
     }
 
@@ -39,6 +49,6 @@ app.post('/account', (req, res) => {
 
     return response.status(201).send();
 
-}) 
+})
 
 app.listen(3306); 
